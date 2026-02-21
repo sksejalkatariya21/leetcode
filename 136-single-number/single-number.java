7dcf71fa7255d5385b05a08302204fffba3be870
katarse <sksejalkatariya21@gmail.com>
2026-02-21 12:57:35 +0530

    Time: 1 ms (99.89%) | Memory: 47.3 MB (13.50%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,18 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> freqOne = new HashMap<Integer, Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(freqOne.containsKey(nums[i])){
-                freqOne.put(nums[i],freqOne.get(nums[i])+1);
-            }else{
-            freqOne.put(nums[i],1);
-        }
-        }
-        for(Map.Entry<Integer, Integer> entry : freqOne.entrySet() ){
-            if(entry.getValue().equals(1)){
-                return entry.getKey();
-            }
-        }
-        return 0;
+        int result = 0;
+       for(int i=0;i<nums.length;i++){
+            result = result ^nums[i];
+       }
+       return result;
     }
 }
