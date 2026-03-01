76bf90d9f5e03ae69fcb76fa729c773905fddc0a
katarse <sksejalkatariya21@gmail.com>
2026-03-01 12:37:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (86.59%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        for(int i=2;i<=n;i++){
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
